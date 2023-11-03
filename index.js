3827f7332f7414bb8bfaa1ae3891236248664aa8
quinn woodhouse <qpwoodhouse@gmail.com>
2023-11-03 14:46:49 -0600

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(function(game) {
+      return game.result === "W";
+    });
+  
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
+  
